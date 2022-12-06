60aec023daa01bb97fc72b786e1cb03de519e618
Dragan Bozhinovski <Dragan.Bozhinovski@mandg.com>
2022-12-06 15:50:51 +0100

    Azure blob storage clustering

--- StockMarket.SymbolService/Program.cs
+++ StockMarket.SymbolService/Program.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Orleans;
+using Orleans.Configuration;
 using Orleans.Hosting;
+using System.Net;
 
 await Host.CreateDefaultBuilder(args)
     .UseOrleans(siloBuilder =>
     {
         siloBuilder
-            .UseLocalhostClustering()
+            .ConfigureEndpoints(IPAddress.Parse("127.0.0.1"), 11111, 30000)
+            .Configure<ClusterOptions>(options =>
+            {
+                options.ClusterId = "StockSymbol";
+                options.ServiceId = "StockSymbolService";
+            })
+            .UseAzureStorageClustering(options =>
+            {
+                options.ConfigureTableServiceClient("DefaultEndpointsProtocol=https;AccountName=stocksymbolservice;AccountKey=aM+XKkRgwu0ACb2kF8511TdmA2+B5IQxen5CIZVEEOq5Om0Lj1pTOmRXvnPt6e6p/vMMwHGoliAN+ASttOoYNg==;BlobEndpoint=https://stocksymbolservice.blob.core.windows.net/;QueueEndpoint=https://stocksymbolservice.queue.core.windows.net/;TableEndpoint=https://stocksymbolservice.table.core.windows.net/;FileEndpoint=https://stocksymbolservice.file.core.windows.net/;");
+            })
             .UseDashboard(x =>
             {
                 x.HostSelf = true;
@@ -14,12 +25,12 @@ await Host.CreateDefaultBuilder(args)
                 x.Host = "*";
                 x.CounterUpdateIntervalMs = 1000;
             })
-            .AddAzureTableGrainStorage(
+            .AddAzureBlobGrainStorage(
                     name: "profileStore", 
                     configureOptions: options =>
                     {
                         options.UseJson = true;
-                        options.ConfigureTableServiceClient("DefaultEndpointsProtocol=https;AccountName=stocksymbolservice;AccountKey=aM+XKkRgwu0ACb2kF8511TdmA2+B5IQxen5CIZVEEOq5Om0Lj1pTOmRXvnPt6e6p/vMMwHGoliAN+ASttOoYNg==");
+                        options.ConfigureBlobServiceClient("DefaultEndpointsProtocol=https;AccountName=stocksymbolservice;AccountKey=aM+XKkRgwu0ACb2kF8511TdmA2+B5IQxen5CIZVEEOq5Om0Lj1pTOmRXvnPt6e6p/vMMwHGoliAN+ASttOoYNg==");
                     }
             );
 
